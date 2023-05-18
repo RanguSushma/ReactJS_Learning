9b65ef650673026f1c7b73e77621c4e7c25b2046
Rangu Sushma <rangu.sushmma2610@gmail.com>
2023-05-18 12:50:38 +0530

    Filter commit

--- secondapp/src/container/App.js
+++ secondapp/src/container/App.js
@@ -9,16 +9,25 @@ class App extends Component{
     constructor(props){
         super(props)
             this.state={
-                productData:JSON
+                productData:JSON,
+                filterData:JSON
             }
         
+    }
+    filterData = (keyword) =>{   
+        {/*filterData is a function with user input(Keyword) output should not redeclare but can reassign updated values*/}
+        let output=this.state.productData.filter((data)=>{
+        {/*first we filter producData, then that data with name and that should be indexvalue. finally that will return true or false. Iteration occur with every array*/}
+            return data.name.toLowerCase().indexOf(keyword.toLowerCase())>-1
+        })
+        this.setState({filterData:output})
     }
    
     render(){
         return(
             <div>
-            <Header userInput={(data)=>{console.log("in parent",data)}} /> {/*user imput is a property*/}
-            <ProductDisplay products={this.state.productData}/>
+            <Header userInput={(data)=>{this.filterData(data)}} /> {/*user imput is a property*/}
+            <ProductDisplay products={this.state.filterData}/>
             <Footer year='2023' month='May' /> 
             {/* user defined attributes or properties can be passed from nested component from parent to child and done binding */}
         </div>
