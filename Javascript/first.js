ce87bcfe79f38500583dc198544f66574eedcb10
Rangu Sushma <rangu.sushmma2610@gmail.com>
2023-05-20 00:48:12 +0530

    Javascript more practice changes

--- Javascript/first.js
+++ Javascript/first.js
@@ -1,6 +1,60 @@
-// **dispalying array elements using rest arguments
+ES5
+function oldVersion(){
+    console.log('oldversion')
+}
+oldVersion()
+oldversion
+
+function oldVersion(a){
+    console.log(a+40)
+}
+oldVersion(110)
+150
+
+ES6
+const newVersion=()=>{
+    console.log('newVersion');
+}
+newVersion();
+newVersion
+
+const newVersionn=(a,b)=>{
+    console.log(a+b);
+}
+newVersionn(40,60);
+100
+
+// const newVersionn=a,b=>console.log(a+b);
+// newVersionn(40,60);
+//  Uncaught SyntaxError: Missing initializer in const declaration
+
+const newwVersionn=a=>console.log(a+60);
+newwVersionn(20);
+80
+
+const neVersionn=a=> a+60;
+console.log(neVersionn(20));
+// we dont need to use return, that curly braces and function braces in ES6
+80
+
+// --------------------------------------------------------
+
+// **dispalying array elements using REST OPERATOR(...args) this assigns rest elements to automatically
 // function add(...args){
 //     console.log(args)
+//     let sum=0;
+//     for(int i=0;i<args.length;i++){
+//         sum+=args[i]
+//     }
+//     return sum
+// }
+// add(1,2,5,7,8)
+// [1,2,5,7,8]  ans
+//---------------------------------------------
+
+// **dispalying array elements using REST OPERATOR(...args) this assigns rest elements to automatically
+// function add(a,...args){
+//     console.log(args)   **a variable will asign to 1 rest of the args will assign automatically and prints rest operator only
 //     let sum=0;
 //     for(int i=0;i<args.length;i++){
 //         sum+=args[i]
@@ -9,12 +63,34 @@
 
 // }
 // add(1,2,5,7,8)
-// [1,2,5,7,8]  ans
+//[2,5,7,8]
 // ---------------------------
+
 // var a=[1,2,3,6]
 // var b=[7,8,9]
-// var a=[1,2,...b,3,6] // adding two arrays using ...var
+// var a=[1,2,...b,3,6] // adding two arrays using ...var(SPEAD OPERATOR)
 // ----------------------------
+
+// adding two objects using SPEAD OPERATOR
+// obj1={
+//     name:'sushma',
+//     age:'23',
+// }
+// obj2={
+//     ...obj1,
+//     mail:"sushma@gmail.com"
+// }
+// console.log(obj2);
+// VM2953:9 {name: 'sushma', age: '23', mail: 'sushma@gmail.com'}
+// ---------------------------------------------------------------------
+
+//destructuring objects in JS7
+// values=[1,2,3]
+// [a,b,r]=values
+// console.log(a,b,r);
+// [1,2,3]
+
+//-----------------------------------------------
 // **adding array to arguments
 // function add(...args){
 //     console.log(args)
